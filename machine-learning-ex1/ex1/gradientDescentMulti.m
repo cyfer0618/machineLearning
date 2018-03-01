4c5add934ce1977cedeedd28f8f74b38de7dd261
Rishabh Kumar <kr.rishabh618@gmail.com>
2018-03-02 03:07:12 +0530

    Completed Ex1

--- machine-learning-ex1/ex1/gradientDescentMulti.m
+++ machine-learning-ex1/ex1/gradientDescentMulti.m
@@ -16,16 +16,8 @@ for iter = 1:num_iters
     % Hint: While debugging, it can be useful to print out the values
     %       of the cost function (computeCostMulti) and gradient here.
     %
-
-    invX = X.';
-    theta = (pinv(invX * X))*invX*y;
-
-
-
-
-
-
-
+    
+    theta = theta - (alpha/m) * (X' * (X * theta - y));
 
     % ============================================================
 
